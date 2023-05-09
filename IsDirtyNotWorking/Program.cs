0947afd628a6e5fc24b2cc965d372592bc3452be
Geoffray Schmetz <geoffray.schmetz@myocene.com>
2023-05-09 12:11:28 +0200

    working issue demo

--- IsDirtyNotWorking/Program.cs
+++ IsDirtyNotWorking/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+
+
+
+using (var gitCommandProcess = new Process())
+{
+    gitCommandProcess.StartInfo.FileName = "git";
+    gitCommandProcess.StartInfo.Arguments = " diff --quiet HEAD";
+    gitCommandProcess.StartInfo.CreateNoWindow = true;
+    gitCommandProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+    gitCommandProcess.StartInfo.UseShellExecute = true;
+    gitCommandProcess.Start();
+    gitCommandProcess.WaitForExit();
+
+    Console.WriteLine($"ThisAssembly.Git.IsDirty={ThisAssembly.Git.IsDirty}");
+    Console.WriteLine($"git diff --quiet HEAD={gitCommandProcess.ExitCode == 1}");
+}
+
+Console.ReadKey();
